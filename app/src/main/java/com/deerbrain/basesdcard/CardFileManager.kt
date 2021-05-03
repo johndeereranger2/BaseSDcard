86ae35d55326125ed1c3cfe78180a694740e703b
Byron Smith <inadeerbrain@gmail.com>
2021-05-03 18:34:39 -0400

    added Map

--- app/src/main/java/com/deerbrain/basesdcard/CardFileManager.kt
+++ app/src/main/java/com/deerbrain/basesdcard/CardFileManager.kt
@@ -1,5 +1,6 @@
 package com.deerbrain.basesdcard
 
+import android.media.Image
 import android.os.Build
 import java.text.SimpleDateFormat
 import java.time.LocalDate
@@ -18,6 +19,8 @@ class CardFileManager {
 //        return itemsFromSDCard
 //    }
 
+    var parentDirectory:
+
     fun getFilesWithPath(path: String) : MutableList<FileItem> {
         var itemsFromSDCard = mutableListOf<FileItem>()
         var date = Date()
@@ -33,4 +36,48 @@ class CardFileManager {
         return itemsFromSDCard
     }
 
+    fun getPicsAtPath(path: String) : MutableList<FileItem> {
+        //goes through card and builds up an array of FileItems that are only pictures
+
+    }
+
+    fun getMoviesAtPath(path: String) : MutableList<FileItem> {
+        //goes through card and builds up an array of FileItems that are only Movies
+
+    }
+
+
+    fun receivedDeviceStatusChanged(status: DeviceStatus) {
+        //this may take a parameter
+
+        when (status) {
+            success -> {
+                //notify status is connected
+            }
+            errorStatus -> {
+                //reportouterr
+            }
+            disconnect -> {
+                //report disconnection
+            }
+        }
+
+    }
+
+    fun removeFilesFromPaths(paths: ArrayList<String>) -> Boolean {
+        //remove multiple files from the SD card reader and return true if successful or return some value of success or error
+    }
+
+
+    fun removeFileFromPath(path: String) -> Bool {
+        //remove single file from SD card Reader
+    }
+
+    fun showImage(isBig: Boolean = false, fileName: String, atDirectory: String) : Image {
+        // this will take a file name at a directory and return an image.
+
+        //if isBIG is true return full size image
+        //if isBig is false return a rezied Image to CGSize of 60x60
+
+    }
 }
--- app/src/main/java/com/deerbrain/basesdcard/Maps/MapCacheTileProvider.kt
+++ app/src/main/java/com/deerbrain/basesdcard/Maps/MapCacheTileProvider.kt
@@ -1,8 +1,9 @@
 package com.deerbrain.basesdcard.Maps
 
 import com.google.android.gms.maps.model.Tile
+import com.google.android.gms.maps.model.TileProvider
 
-class MapCacheTileProvider {
+class MapCacheTileProvider : TileProvider {
     override fun getTile(x: Int, y: Int, z: Int): Tile {
         return MapCacheManager.getTile(x,y,z)
     }
